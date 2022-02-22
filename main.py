dc9e78ec709eceaf63473ca09f089990c9c8ab6e
lukepawlicki <98533140+lukepawlicki@users.noreply.github.com>
2022-02-22 23:53:05 +0000

    first draft

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
 import turtle
-
+import math
 ########### Your Code here ##############
 # You should only have functions here
 # If you have anything outside of a function, 
 # then you do not fully understand functions
 # and should review how they work or ask for help
+def drawSineCurve(dart):
+  for x in range(-360,361):
+    y = math.sin(math.radians(x))
+    dart.goto(x,y)
+def setupWindow(wn):
+  wn.bgcolor("light blue")
+  wn.setworldcoordinates(-360,-1,360,1)
+def setupAxis(dart):
+  dart.up()
+  dart.goto(0,10)
+  dart.down()
+  dart.goto(0,-10)
+  dart.up()
+  dart.goto(-500,0)
+  dart.down()
+  dart.goto(500,0)
+  dart.up
+  dart.goto(-360,0)
+  dart.down
+def drawCosineCurve(dart):
+  dart.up()
+  dart.goto(-360,1)
+  dart.down()
+  for x in range(-360,361):
+    y = math.cos(math.radians(x))
+    dart.goto(x,y)
+def drawTangentCurve(dart):
+  dart.up()
+  dart.goto(-360,0)
+  dart.down()
+  for x in range(-360,361):
+    y = math.tan(math.radians(x))
+    dart.goto(x,y)
+    
+
+
+
+
+
+
 
 
 
@@ -21,7 +61,6 @@ def main():
     dart = turtle.Turtle()
     dart.speed(0)
     drawSineCurve(dart)
-
     #Part B
     setupWindow(wn)
     setupAxis(dart)
@@ -29,5 +68,8 @@ def main():
     drawSineCurve(dart)
     drawCosineCurve(dart)
     drawTangentCurve(dart)
+  
     wn.exitonclick()
+
+   
 main()
